b75108391f6fe37a8f20a4a4a09ddc8c17950321
agu <iba.agustin@gmail.com>
2023-07-13 20:06:18 -0300

    added oauth2 credentials

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const unsubsRouter = require("./controllers/unsubs");
+const dotenv = require("dotenv").config();
+const { google } = require("googleapis");
 
 const app = express();
 
 const port = 3000;
+
+const auth2Client = new google.auth.OAuth2(
+  process.env.client_id,
+  process.env.client_secret,
+  process.env.redirect_uri
+);
+
+console.log(auth2Client);
 
 app.get("/", (req, res) => {
   res.status(200).send("hey world");
